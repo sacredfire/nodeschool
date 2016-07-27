ae6056aac10647c282a47f3f44728b183560f1d2
Sergei Kissel <sergei.kissel@gmail.com>
2016-07-27 08:54:59 +0300

    amend map in funcjs

--- functional-js/map.js
+++ functional-js/map.js
@@ -1,8 +1,19 @@
-    function doubleAll(numbers) {
-      var result = numbers.map(function (num){
-        return num * 2;
-      })
-      return result
-    }
+function doubleAll (numbers) {
+  var result = numbers.map(function (num) {
+    return num * 2
+  })
+  return result
+}
 
-    module.exports = doubleAll
+module.exports = doubleAll
+
+// function quadroupleAll (numbers) {
+//   var result = numbers.map(function (num) {
+//     return num * 4
+//   })
+//   return result
+// }
+
+// var numArr = [2, 3, 5, 10]
+
+// console.log(quadroupleAll(numArr))
