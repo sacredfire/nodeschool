c593ec8d62caccce1ee3db15bd013d8fe51a0ca9
Sergei Kissel <sergei.kissel@gmail.com>
2016-08-11 08:59:35 +0300

    every_some

--- functional-js/every.js
+++ functional-js/every.js
@@ -1,9 +1,9 @@
-module.exports = function checkUsersValid(goodUsers) {
-  return function(submittedUsers) {
-      return !submittedUsers.some(function(submittedUser){
-          return !goodUsers.some(function(goodUser){
-              return goodUser.id == submittedUser.id
-          })
+module.exports = function checkUsersValid (goodUsers) {
+  return function (submittedUsers) {
+    return !submittedUsers.some(function (submittedUser) {
+      return !goodUsers.some(function (goodUser) {
+        return goodUser.id === submittedUser.id
       })
-  };
+    })
+  }
 }
