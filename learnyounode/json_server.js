19b84dd84a5100ae73fc6261f41d5b1715d3cb19
sacredfire <sergei.kissel@gmail.com>
2016-09-29 19:36:26 +0300

    json_server standard spelling

--- learnyounode/json_server.js
+++ learnyounode/json_server.js
@@ -1,40 +1,39 @@
 const http = require('http')
-    , url = require('url');
+const url = require('url')
 
-var port = process.argv[2];
-var pathToAPI = '/api/parsetime';
-var pathToAPI1 = '/api/unixtime';
+var port = process.argv[2]
+var pathToAPI = '/api/parsetime'
+var pathToAPI1 = '/api/unixtime'
 
-var server = http.createServer(function(req, res) {
-    
-    var parsedUrl = url.parse(req.url);
-    var arrISOfull = (parsedUrl.query).split('=');
-    var dateStrUnix = Date.parse(arrISOfull[1]);
-    var dateObj = new Date(dateStrUnix);
+var server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url)
+  var arrISOfull = (parsedUrl.query).split('=')
+  var dateStrUnix = Date.parse(arrISOfull[1])
+  var dateObj = new Date(dateStrUnix)
 
-    var time = {};
-        time.hour = dateObj.getHours();
-        time.minute = dateObj.getMinutes();
-        time.second = dateObj.getSeconds();
+  var time = {}
+  time.hour = dateObj.getHours()
+  time.minute = dateObj.getMinutes()
+  time.second = dateObj.getSeconds()
 
-    var unixtime = {}
-        unixtime.unixtime = dateStrUnix;
+  var unixtime = {}
+  unixtime.unixtime = dateStrUnix
 
-    var pathName = (parsedUrl.pathname);
+  var pathName = (parsedUrl.pathname)
 
-    if (pathName == pathToAPI) {
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(time))
-    }
-    
-    else if (pathName == pathToAPI1) {
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(unixtime))
-    }
-    else {
-        res.writeHead(404)
-        res.end()
-    }
-});
+  if (pathName === pathToAPI) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(time))
+  }
+
+  else if (pathName === pathToAPI1) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(unixtime))
+  }
+  else {
+    res.writeHead(404)
+    res.end()
+  }
+})
 
 server.listen(port)
