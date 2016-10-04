17827cf261651f02c2d9c9fa5e8d4013eea243d8
sacredfire <sergei.kissel@gmail.com>
2016-10-04 10:19:13 +0300

    small edits

--- learnyounode/collect_http.js
+++ learnyounode/collect_http.js
@@ -3,14 +3,14 @@ var bl = require('bl')
 
 var url = "http://www.google.com/index.html"
 if (process.argv[2] !== undefined) {
-    url = process.argv[2];
 }
+    url = process.argv[2]
 
 http.get(url, function(res) {
 
     res.pipe(bl(function(err, data) {
         if (err)
-            return console.error('There was an error', err);
+            return console.error('There was an error', err)
         data = data.toString()
         console.log(data.length)
         console.log(data)
