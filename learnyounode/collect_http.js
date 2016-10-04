6d865ad4945b59a7920c0ac758ff6cfe5fcc8aeb
sacredfire <sergei.kissel@gmail.com>
2016-10-04 11:10:31 +0300

    http_collect test

--- learnyounode/collect_http.js
+++ learnyounode/collect_http.js
@@ -1,26 +1,26 @@
 var http = require('http')
 var bl = require('bl')
 
-var url = "http://www.google.com/index.html"
+var url = 'http://www.google.com/index.html'
+
 if (process.argv[2] !== undefined) {
+  url = process.argv[2]
 }
-    url = process.argv[2]
 
-http.get(url, function(res) {
+http.get(url, function (res) {
+  res.pipe(bl(function (err, data) {
+    if (err)
+      return console.error('There was an error', err)
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
 
-    res.pipe(bl(function(err, data) {
-        if (err)
-            return console.error('There was an error', err)
-        data = data.toString()
-        console.log(data.length)
-        console.log(data)
-    }))
+  res.on('error', function (e) {
+    return console.log('Got an error here: ' + error.message)
+  })
 
-    res.on("error", function(e){
-        return console.log("Got an error here: " + error.message)
-    })
-
-    res.on("end", function(){
-    //    return console.log("End")
-    })
+  res.on('end', function () {
+//    return console.log("End")
+  })
 })
