a94e2bc4d4a5b45347388f9f1d696cae163e9fb4
sacredfire <sergei.kissel@gmail.com>
2016-10-19 20:02:01 +0300

    juggling mods

--- learnyounode/juggling.js
+++ learnyounode/juggling.js
@@ -1,27 +1,25 @@
 var http = require('http')
-var bl =require('bl')
+var bl = require('bl')
 var results = []
 var count = 0
 
-for (var i = 0; i <3; i++)
-    httpGet(i)
-
-function httpGet(index){
-    http.get(process.argv[2 + index], function(res) {
-        res.pipe(bl(function(err, data) {
-            if (err)
-                return console.error(err)
-
-            results[index] = data.toString()
-            count++
-
-            if (count == 3)
-                _printResults()
-        }))
-    })
+for (var i = 0; i < 3; i++) {
+  httpGet(i)
 }
 
-function _printResults() {
-    for (var i = 0; i < 3; i++)
-        console.log(results[i])
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (res) {
+    res.pipe(bl(function (err, data) {
+      if (err) return console.error(err)
+      results[index] = data.toString()
+      count++
+      if (count === 3) _printResults()
+    }))
+  })
+}
+
+function _printResults () {
+  for (var i = 0; i < 3; i++) {
+    console.log(results[i])
+  }
 }
