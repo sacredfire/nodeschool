5805c5aa3aa2f265d21cec41c95dfe0cedd97a17
sacredfire <sergei.kissel@gmail.com>
2016-10-21 23:44:32 +0300

    re1_uppercaserer_alt

--- learnyounode/re1_uppercaserer.js
+++ learnyounode/re1_uppercaserer.js
@@ -1,13 +1,26 @@
 var http = require('http')
 var map = require('through2-map')
 
+// var server = http.createServer(function (req, res) {
+//   if (req.method !== 'POST') {
+//     return res.end('Pls supply POST request')
+//   }
+//   req.pipe(map(function (chunk) {
+//     return chunk.toString().toUpperCase()
+//   })).pipe(res)
+// })
+
 var server = http.createServer(function (req, res) {
   if (req.method !== 'POST') {
     return res.end('Pls supply POST request')
   }
-  req.pipe(map(function (chunk) {
-    return chunk.toString().toUpperCase()
-  })).pipe(res)
+  var body = ''
+  req.on('error', console.error)
+  req.on('data', function (chunk) {
+    body += chunk.toString()
+  }).on('end', function () {
+    res.write(body.toUpperCase())
+  })
 })
 
 server.listen(Number(process.argv[2]))
